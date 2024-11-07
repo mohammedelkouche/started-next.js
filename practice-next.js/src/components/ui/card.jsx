c888d2a5443219dca9855fc3e597e169e7cd729b
Mohammed El Kouche <mohammed0elkouche@gmail.com>
2024-11-08 00:25:05 +0100

    clean som folders

--- practice-next.js/src/components/ui/card.jsx
+++ practice-next.js/src/components/ui/card.jsx
@@ -1,23 +1,6 @@
 // import React from 'react';
 import "@/styles/globals.css";
 
-
-// export function Card({ children, className }) {
-//   return (
-//     <div className={`bg-white shadow-md rounded-lg ${className}`}>
-//       {children}
-//     </div>
-//   );
-// }
-
-// export function CardContent({ children, className }) {
-//   return (
-//     <div className={`p-4 ${className}`}>
-//       {children}
-//     </div>
-//   );
-// }
-
 import React from 'react';
 
 export function Card({ children, className }) {
